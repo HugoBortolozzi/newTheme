c764b3d27353d2b98157c6fff92f32b3a81d421c
Hugo Bortolozzi <hugo_bortolozzi@yahoo.fr>
2019-08-23 10:59:38 +0200

    Dans le menu on rend le titre dynamique grâce à get_bloginfo('name')

--- header.php
+++ header.php
@@ -14,7 +14,7 @@ wp_head();
   <body>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark ftco-navbar-light" id="ftco-navbar">
 	    <div class="container">
-	      <a class="navbar-brand" href="index.html">Stories<span>.</span></a>
+	      <a class="navbar-brand" href="index.html"><?php echo get_bloginfo('name'); ?><span>.</span></a>
 	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
 	        <span class="oi oi-menu"></span> Menu
 	      </button>
